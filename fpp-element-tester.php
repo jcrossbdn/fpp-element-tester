8184f61245b64d45968f8b65218eb8f2e7d2864b
Jason Cross <jcrossbdn@users.noreply.github.com>
2014-12-14 14:24:23 -0600

    Update fpp-element-tester.php

--- fpp-element-tester.php
+++ fpp-element-tester.php
@@ -1,6 +1,7 @@
 <script>
   function getElements(command) {
     var screenPosition = $(window).scrollTop();
+    
     $.ajax({
       type: "GET",
       url: "?plugin=fpp-element-tester&page=ajax.php&nopage=1" + command,
@@ -14,6 +15,13 @@
   $(document).ready(function() {
     getElements('');
   });
+  
+  $(document).ajaxStart(function() {
+    $("#ajaxLoading").show();
+  });
+  $(document).ajaxStop(function() {
+    $("#ajaxLoading").hide();
+  });
 </script>
 
 <div id="elements" class="settings">
@@ -22,3 +30,5 @@
 <div id='elementBlock'></div>
 </fieldset>
 </div>
+
+<div id="ajaxLoading" class="ajaxLoading" title="Loading..."><img src="?plugin=fpp-element-tester&page=loading.gif&nopage=1" alt="Loading..."></div>
